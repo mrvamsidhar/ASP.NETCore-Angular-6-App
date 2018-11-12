244a59c5fd062ee1d6e0bf43b45ee39017bee652
Balachandra Mamillapalli <Balachandra.Mamillapalli@appliedis.com>
2018-11-11 22:23:46 -0500

    End of section4

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,16 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class ValueComponent implements OnInit {
-  values: any;
-  constructor(private http: HttpClient) {}
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  values: any ;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getValues();
+  }
+
+  registertoggle() {
+    this.registerMode = true;
   }
 
   getValues() {
@@ -22,4 +27,9 @@ export class ValueComponent implements OnInit {
        console.log(error) ;
     } );
   }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
+
 }
